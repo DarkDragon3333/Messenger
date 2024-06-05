9456eda50ada2bca3fda19146e4ac2a6fb164df9
DarkDragon3333 <minaichiev-dragon2017@yandex.ru>
2024-06-05 19:04:18 +0300

    Не отметил файл в предыдущем коммите

--- app/src/main/java/com/example/messenger/screens/Contacts.kt
+++ app/src/main/java/com/example/messenger/screens/Contacts.kt
@@ -3,14 +3,14 @@ package com.example.messenger.screens
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import com.example.messenger.screens.componentOfScreens.ContactCard
-import com.example.messenger.utilsFilies.contactsList
+import com.example.messenger.utilsFilies.contactsListUSER
 import com.example.messenger.utilsFilies.mapContacts
 
 @Composable
 fun ContactsScreen() {
     LazyColumn {
         items(mapContacts.size){contact ->
-            ContactCard(mapContacts[contactsList[contact]])
+            ContactCard(mapContacts[contactsListUSER[contact]])
         }
     }
 }
